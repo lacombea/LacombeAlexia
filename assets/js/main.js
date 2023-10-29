f7c1e3b8d53b79c901921becf7e65964f44f9bcc
Alexia <alacombe1@etu.uqac.ca>
2023-10-29 14:42:37 -0400

    Refonte complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,92 +33,94 @@ function onClick(element) {
 		});
 
 	// Nav.
-		var $nav = $('#nav');
+	var $nav = $('#nav');
 
-		if ($nav.length > 0) {
 
-			// Shrink effect.
-				$main
-					.scrollex({
-						mode: 'top',
-						enter: function() {
-							$nav.addClass('alt');
-						},
-						leave: function() {
-							$nav.removeClass('alt');
-						},
-					});
+	if ($nav.length > 0) {
 
-			// Links.
-				var $nav_a = $nav.find('a');
+		// Shrink effect.
+			$main
+				.scrollex({
+					mode: 'top',
+					enter: function() {
+						$nav.addClass('alt');
+					},
+					leave: function() {
+						$nav.removeClass('alt');
+					},
+				});
 
-				$nav_a
-					.scrolly({
-						speed: 1000,
-						offset: function() { return $nav.height(); }
-					})
-					.on('click', function() {
+		// Links.
+			var $nav_a = $nav.find('a');
 
-						var $this = $(this);
+			$nav_a
+				.scrolly({
+					speed: 1000,
+					offset: function() { return $nav.height(); }
+				})
+				.on('click', function() {
 
-						// External link? Bail.
-							if ($this.attr('href').charAt(0) != '#')
-								return;
+					var $this = $(this);
 
-						// Deactivate all links.
-							$nav_a
-								.removeClass('active')
-								.removeClass('active-locked');
+					// External link? Bail.
+						if ($this.attr('href').charAt(0) != '#')
+							return;
 
-						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-							$this
-								.addClass('active')
-								.addClass('active-locked');
+					// Deactivate all links.
+						$nav_a
+							.removeClass('active')
+							.removeClass('active-locked');
 
-					})
-					.each(function() {
+					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+						$this
+							.addClass('active')
+							.addClass('active-locked');
 
-						var	$this = $(this),
-							id = $this.attr('href'),
-							$section = $(id);
+				})
+				.each(function() {
 
-						// No section for this link? Bail.
-							if ($section.length < 1)
-								return;
+					var	$this = $(this),
+						id = $this.attr('href'),
+						$section = $(id);
 
-						// Scrollex.
-							$section.scrollex({
-								mode: 'middle',
-								initialize: function() {
+					// No section for this link? Bail.
+						if ($section.length < 1)
+							return;
 
-									// Deactivate section.
-										if (browser.canUse('transition'))
-											$section.addClass('inactive');
+					// Scrollex.
+						$section.scrollex({
+							mode: 'middle',
+							initialize: function() {
 
-								},
-								enter: function() {
+								// Deactivate section.
+									if (browser.canUse('transition'))
+										$section.addClass('inactive');
 
-									// Activate section.
-										$section.removeClass('inactive');
+							},
+							enter: function() {
 
-									// No locked links? Deactivate all links and activate this section's one.
-										if ($nav_a.filter('.active-locked').length == 0) {
+								// Activate section.
+									$section.removeClass('inactive');
 
-											$nav_a.removeClass('active');
-											$this.addClass('active');
+								// No locked links? Deactivate all links and activate this section's one.
+									if ($nav_a.filter('.active-locked').length == 0) {
 
-										}
+										$nav_a.removeClass('active');
+										$this.addClass('active');
 
-									// Otherwise, if this section's link is the one that's locked, unlock it.
-										else if ($this.hasClass('active-locked'))
-											$this.removeClass('active-locked');
+									}
 
-								}
-							});
+								// Otherwise, if this section's link is the one that's locked, unlock it.
+									else if ($this.hasClass('active-locked'))
+										$this.removeClass('active-locked');
 
-					});
+							}
+						});
 
-		}
+				});
+
+	}
+
 
 	// Scrolly.
 		$('.scrolly').scrolly({
